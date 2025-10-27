7ad83172bb6eaf727b55b9c10315f3059d940f54
João Kiakumbo <jkiakumbo@icloud.com>
2025-10-27 22:41:59 +0000

    Structure the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,47 @@
 plugins {
-    kotlin("jvm") version "2.2.20"
+    java
+    id("org.springframework.boot") version "3.5.7" apply false
+    id("io.spring.dependency-management") version "1.1.7"
+    kotlin("jvm") version "2.2.21" apply false
+    kotlin("plugin.spring") version "2.2.21" apply false
 }
 
-group = "dev.jkiakumbo.ccm"
-version = "1.0-SNAPSHOT"
+allprojects {
+    group = "dev.jkiakumbo.paymentorchestrator"
+    version = "1.0.0"
 
-repositories {
-    mavenCentral()
+    repositories {
+        mavenCentral()
+    }
 }
 
-dependencies {
-    testImplementation(kotlin("test"))
-}
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "kotlin")
+    apply(plugin = "kotlin-spring")
 
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(21)
+    java {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(21)
+        }
+    }
+
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter-web")
+        implementation("org.springframework.boot:spring-boot-starter-actuator")
+        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+        implementation("org.springframework.boot:spring-boot-starter-validation")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        implementation("org.springframework.kafka:spring-kafka:3.3.3")
+        implementation("org.springframework.statemachine:spring-statemachine-core:4.0.0")
+        implementation("io.micrometer:micrometer-tracing-bridge-brave")
+        implementation("io.zipkin.reporter2:zipkin-reporter-brave")
+        runtimeOnly("org.postgresql:postgresql:42.7.4")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testImplementation("org.springframework.kafka:spring-kafka-test")
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,9 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 rootProject.name = "payment-orchestrator"
+include("services")
+include("services:common")
+include("services:fraud-service")
+include("services:funds-service")
+include("services:processor-service")
+include("services:ledger-service")
